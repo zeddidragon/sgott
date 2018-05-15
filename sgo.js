7c7b02383e39cd2c51d1ee08749f54445684d163
Tony Sæle <tony@globalit.no>
2018-05-15 05:36:16 +0200

    can convert SGO to JSON

--- sgo.js
+++ sgo.js
@@ -3,14 +3,14 @@ var remainder
 var endian
 
 const types = {
-  0: ['ptr', UInt],
+  0: ['ptr', UInt, Pointer],
   1: ['int', Int],
   2: ['float', Float],
-  3: ['string', UInt, StrPointer, str => str],
+  3: ['string', UInt, StrPointer],
 }
 
 function Str(buffer) {
-  return buffer.toString()
+  return buffer.toString().replace(/\0/g, '')
 }
 
 function UInt(buffer) {
@@ -26,6 +26,12 @@ function Float(buffer) {
 }
 
 var visited = new Set()
+
+function Pointer(buffer, index, size) {
+  if(!size) return null
+  return consume({mode: 'values'}, buffer, index, size).variables
+}
+
 function StrPointer(buffer, index, size) {
   visited.add(index)
   return Str(buffer.slice(index, index + size * 2))
@@ -34,53 +40,36 @@ function StrPointer(buffer, index, size) {
 const chomp = {
   header: (state, data, index) => {
     const header = Str(data.slice(index, index + 8))
-    endian = header === '\0SGO' ? 'LE' : 'BE'
-    console.log(`${header} (${endian})`)
+    endian = header === 'SGO' ? 'LE' : 'BE'
     state.structValues = UInt(data.slice(index + 16, index + 20))
     state.structSize = index + 4 + UInt(data.slice(index + 28, index + 32))
-    console.log(`values: ${state.structValues}`)
-    console.log(`size: ${state.structSize}`)
-    console.log(`=============================================================`)
     state.mode = 'values'
     return index + 32
   },
-  values: (state, data, index, depth) => {
+  values: (state, data, index) => {
     state.consumed++
     const slice = data.slice(index, index + 4)
-    const [type, transform, getPointed, renderPointed] = types[UInt(slice)] || []
+    const [type, transform, getPointed] = types[UInt(slice)] || []
     if(!type) {
-      console.error(index.toString(16), `Type not found: `, data.slice(index, index + 12))
+      state.variables.push({
+        type: 'unknown',
+        value: slice.toString('base64'),
+      })
       return index + 12
     }
-    const isPointer = ['ptr',' struct'].includes(type)
-    if(type === 'struct') return index + 12
+    const isPointer = type === 'ptr'
     const size = UInt(data.slice(index + 4, index + 8))
     const valueSize = 4
     const end = index + 8 + valueSize
     const valueChunk = data.slice(index + 8, end)
     const value = transform(data.slice(index + 8, end))
     const pointed = getPointed && getPointed(data, index + value, size)
-    console.log([
-      '  '.repeat(state.depth),
-      ('' + state.consumed).padStart(4),
-      index.toString(16).padStart(4),
-      type.padEnd(5),
-      `(${size}b)`.padStart(6),
-      (type === 'float' ? value.toFixed(2) : (value + '   ')).padStart(16),
-      isPointer && !size && 'NULL',
-      isPointer && size && '{',
-      pointed && `"${renderPointed(pointed)}"`,
-    ]
-      .filter(piece => piece)
-      .join('  ')
-    )
-    if(isPointer && size) {
-      consume({
-        mode: 'values',
-        depth: state.depth + 1,
-      }, data, index + value, size)
-      console.log('  '.repeat(state.depth) + '   }')
-    }
+
+    state.variables.push({
+      type: type,
+      value: isPointer ? pointed : pointed || value,
+    })
+
     if(state.consumed >= state.structValues) {
       state.mode = 'mabHeader'
       return state.structSize
@@ -90,32 +79,28 @@ const chomp = {
   mabHeader: (state, data ,index) => {
     const header = Str(data.slice(index, index + 4))
     state.mode = 'varnames'
-    console.log(`=============================================================`)
-    if(header !== 'MAB\0') return index
-    console.log(header)
-    console.log(`=============================================================`)
+    state.consumed = 0
+    if(header !== 'MAB') return index
+
     // Horrible hack, just jump to what we know is the first variable name
-    
     const varnames = data.indexOf('\0A\0i\0m\0A\0n\0i\0m\0a\0t\0i\0o\0n', index)
     if(!~varnames) throw new Error('AimAnimation not found!')
+    state.mab = data.slice(index, varnames).toString('base64')
     return varnames
   },
   varnames: (state, data, index) => {
     const end = data.indexOf('\0\0', index + 1)
+    if(visited.has(index)) return end + 2
     if(end < 0) {
       state.mode = 'done'
       return
     }
     const label = Str(data.slice(index, end))
-    if(label.length > 6) {
-        console.log([
-        '  ',
-        index.toString(16).padStart(4),
-        label,
-      ].join('  '))
-    } else {
-      console.log(data.slice(index, end + 2), Str(data.slice(index, end)))
+    if(label.length > 3) {
+      state.variables[state.consumed].name = label
+      state.consumed++
     }
+    if(state.consumed >= state.structValues) state.mode = 'done'
     return end + 2
   },
 }
@@ -125,13 +110,18 @@ function consume(state, data, index = 0, values) {
   state.consumed = 0
   state.variables = []
   while(state.mode !== 'done' && (values == null || (state.consumed < values))) {
-    if(index >= size) return
-    if(state.consumed >= values) return
+    if(index >= size) return state
+    if(state.consumed >= values) return state
     index = chomp[state.mode](state, data, index)
   }
-  return state.variables
+  return state
 }
 
 process.stdin.on('data', chunk => {
-  consume({mode: 'header', depth: 0}, chunk)
+  const {variables, mab} = consume({mode: 'header'}, chunk)
+  console.log(JSON.stringify({
+    endian,
+    variables,
+    mab,
+  }, null, 2))
 })
