5bd1a93ff123662e076a468db0fcbd101063f273
Tony A. Sæle <tony.saele@scoutgaminggroup.com>
2019-09-13 14:08:07 +0700

    growth formula for all known growth values

--- helpers/growth.js
+++ helpers/growth.js
@@ -1,18 +1,13 @@
-const formulas = {
-  8: ({ base, zero, exp, lvMin, lvMax }) => { // Damage algorithm
-    const zeroValue = base * zero
-    const growthBase = (base - zeroValue) / Math.pow(5, exp)
-    const ret = Array(lvMax).fill(0)
-    for(var level = lvMin; level <= lvMax; level++) {
-      ret[level] = +(zeroValue + growthBase * Math.pow(level, exp))
-    }
-    
-    return ret
-  },
-}
+function growth({ base, zero, exp, lvMin, lvMax, type, algo }) {
+  const zeroValue = [21, 25, 29].includes(algo) ? base + base * zero : base * zero
+  const growthBase = (base - zeroValue) / Math.pow(5, exp)
+  const ret = Array(lvMax).fill(0)
+  for(var level = lvMin; level <= lvMax; level++) {
+    ret[level] = +(zeroValue + growthBase * Math.pow(level, exp))
+    if(type === 'int') ret[level] = Math.ceil(ret[level])
+  }
 
-function growth(stat) {
-  return formulas[stat.algo](stat)
+  return ret
 }
 
 module.exports = growth
