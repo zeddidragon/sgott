5a95c5328f04b668ca5f3b7b1dca0202703c7a68
Tony A. Sæle <tony.saele@scoutgaminggroup.com>
2019-09-15 13:22:30 +0700

    fix turret params

---
+++ devtools/rename-param.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const allWeapons = require('./all-weapons')
+const getNode = require('../helpers/get-node')
+const meta = require('../sgo-meta').names.ammoClasses.SentryGunBullet01
+
+for(const [ weapon, id ] of allWeapons()) {
+  const type = getNode(weapon, 'AmmoClass').value
+  if(type !== 'SentryGunBullet01') continue
+  const custom = getNode(weapon, 'Ammo_CustomParameter')
+  if(!custom.value) {
+    console.log(getNode(weapon, 'name'))
+  }
+  for(var i = 0; i < meta.length; i++) {
+    const node = custom.value[i]
+    const label = meta[i]
+    if(node.options) delete node.options
+    if(label) {
+      node.name = label
+    } else if(node.name) {
+      delete node.name
+    }
+    console.log(id)
+    console.log(custom.value.map(v => [v.name, v.type, v.value]))
+  }
+  fs.writeFileSync(`data/41/weapons/${id.toUpperCase()}.json`, JSON.stringify(weapon, null, 2))
+}
