1d621f520ec5a469cf19a98b384449e8c0920f89
Tony Sæle <tony.saele@scoutgaminggroup.com>
2021-11-20 09:38:58 +0700

    start on squish script

--- rebalance/rebalance-41.js
+++ rebalance/rebalance-41.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs')
 const json = require('json-stringify-pretty-compact')
-const table = require('../data/41/weapons/_WEAPONTABLE.json')
-const textTable = require('../data/41/weapons/_WEAPONTEXT.json')
+const table = require('../data/41/weapon/_WEAPONTABLE.json')
+const textTable = require('../data/41/weapon/_WEAPONTEXT.json')
 const blurbs = require('../helpers/blurbs')
 const getId = require('../helpers/get-id')
 const getNode = require('../helpers/get-node')
@@ -35,7 +35,7 @@ function rebalance(query, cb) {
       return true
     })
     .forEach((node, i) => {
-      const path = `../data/41/weapons/${getId(node).toUpperCase()}.json`
+      const path = `../data/41/weapon/${getId(node).toUpperCase()}.json`
       const template = require(path)
       const text = textTable.variables[0].value[table.variables[0].value.indexOf(node)]
       modded.add(node)
@@ -571,7 +571,7 @@ rebalance({category: 36, name: /Titan/}, (template, i, meta, text) => {
 
     const path = './SgottMods/weapon/v_404bigtank_mainCannon'
     mainCannonConfig.value = path + '.SGO'
-    const mainCannonTemplate = require('../data/41/weapons/V_404BIGTANK_MAINCANNON')
+    const mainCannonTemplate = require('../data/41/weapon/V_404BIGTANK_MAINCANNON')
     patch(mainCannonTemplate, 'AmmoAlive', 240)
     patch(mainCannonTemplate, 'AmmoSpeed', 20)
     rawSgos.set(path.split(/\//).pop(), mainCannonTemplate)
@@ -661,7 +661,7 @@ rebalance({category: 37, name: /SDL1/}, (template, i, meta, text) => {
 rebalance({category: 37, name: /Grape/}, (template, i, meta, text) => {
   // Add full rotation to grape's cannon
   const path = './SgottMods/weapon/vehicle401_striker'
-  const vehicleTemplate = require('../data/41/weapons/VEHICLE401_STRIKER.json')
+  const vehicleTemplate = require('../data/41/weapon/VEHICLE401_STRIKER.json')
   const cannonControl = getNode(vehicleTemplate, 'striker_cannon_ctrl')
   cannonControl.value[2].value = 60
   rawSgos.set(path.split(/\//).pop(), vehicleTemplate)
@@ -791,7 +791,7 @@ rebalance({category: 39, name:/Proteus/}, (template, i, meta, text) => {
   )
 })
 
-const outDir = './release/sgottstrap/SgottTemplates/weapon'
+const outDir = './release/zeddy41/SgottTemplates/weapon'
 fs.mkdirSync(outDir, { recursive: true })
 for(const [path, template] of rawSgos) {
   const filename = `${outDir}/${path}.json`
@@ -801,7 +801,7 @@ for(const [path, template] of rawSgos) {
 
 for(const node of modded) {
   const id = getId(node)
-  const path = `../data/41/weapons/${id.toUpperCase()}`
+  const path = `../data/41/weapon/${id.toUpperCase()}`
   const template = require(path)
   const text = textTable.variables[0].value[table.variables[0].value.indexOf(node)]
   template.meta = {
---
+++ rebalance/squish-41.js
@@ -0,0 +1,97 @@
+const fs = require('fs')
+const json = require('json-stringify-pretty-compact')
+const table = require('../data/41/weapon/_WEAPONTABLE.json')
+const textTable = require('../data/41/weapon/_WEAPONTEXT.json')
+const config = require('../data/41/CONFIG.json')
+const blurbs = require('../helpers/blurbs')
+const getId = require('../helpers/get-id')
+const getNode = require('../helpers/get-node')
+const patch = require('../helpers/patch')
+
+const rawSgos = new Map()
+const seconds = 60
+
+const modded = new Set()
+
+const difficulties = getNode(config, 'ModeList')
+  .value[0] // Offline mode, arbitrarily
+  .value[6] // Difficulty list
+  .value
+  .slice(1) // Strip Easy from the equation
+  .map(node => node.value[0].value.map(n => n.value / 10))
+
+function replaceText(textNode, pattern, replacement) {
+  textNode.value[3].value = textNode.value[3].value.replace(pattern, replacement)
+}
+
+function pretty(v) {
+  if(v < 3) return v.toFixed(2)
+  if(v < 8) return v.toFixed(1)
+  if(v < 20) return Math.ceil(v)
+  if(v < 90) return Math.ceil(v / 5) * 5
+  if(v < 750) return Math.ceil(v / 10) * 10
+  return Math.ceil(v / 50) * 50
+}
+
+const blacklistNames = RegExp([
+  'Beacon Gun',
+  'Laser Guide',
+  'Plasma Charger',
+  'Plasma Battery',
+  'Plasma Streamer',
+  'Guard Post',
+  'Guard Assist',
+  'Power Assist',
+  'Power Post',
+  'Zone Protector',
+  'Offensive Territory',
+  'Speed Star',
+].join('|'))
+
+for(const node of table.variables[0].value) {
+  const path = `./data/41/weapon/${getId(node).toUpperCase()}.json`
+  const data = JSON.parse(fs.readFileSync(path))
+  const name = getNode(data, 'name').value[1].value
+  const shouldSkip = blacklistNames.test(name)
+  if(shouldSkip) {
+    console.log(`Skipping: ${name}`)
+    continue
+  }
+  const level = node.value[4].value
+  if(level >= 4) continue
+  const rung = difficulties[Math.floor(level)] || [1, 1]
+  const factor = (rung[0] + (rung[1] - rung[0]) * (level % 1))
+  const damageNode = getNode(data, 'AmmoDamage')
+  const oldDamage = damageNode.value
+  const newDamage = pretty(oldDamage / factor)
+  // console.log(getNode(data, 'name').value[1].value.padEnd(48), `${oldDamage} => ${newDamage}`)
+}
+
+/*
+const outDir = './release/squish41/SgottTemplates/weapon'
+fs.mkdirSync(outDir, { recursive: true })
+for(const [path, template] of rawSgos) {
+  const filename = `${outDir}/${path}.json`
+  console.log(`Writing ${filename}` )
+  fs.writeFileSync(filename, json(template))
+}
+
+for(const node of modded) {
+  const id = getId(node)
+  const path = `../data/41/weapons/${id.toUpperCase()}`
+  const template = require(path)
+  const text = textTable.variables[0].value[table.variables[0].value.indexOf(node)]
+  template.meta = {
+    id: id,
+    description: text.value[3].value,
+  }
+  const name = text.value[2]
+    .value
+    .replace(/\s+/g, '-')
+    .replace(/[^0-9a-zA-Z-]/g, '')
+  const filename = `${outDir}/_${id}_${name}.json`
+  console.log(`Writing ${filename}` )
+  fs.writeFileSync(filename, json(template))
+}
+*/
+console.log('Done!')
