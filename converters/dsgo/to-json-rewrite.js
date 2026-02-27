01f4cd81147f03da335b48b88495025422c19667
Tony Sæle <tony.sale@linkmobility.com>
2026-02-27 09:37:14 +0100

    more types

--- converters/dsgo/to-json-rewrite.js
+++ converters/dsgo/to-json-rewrite.js
@@ -1,15 +1,219 @@
-const Mode = { // enum
-  HEADER: 0,
-  VARS: 1,
-  DSGO: 2,
-}
+const util = require('util')
+const kleur = require('kleur')
 
+// enum used for both DSGO type and the parser's state machine
 const DsgoType = {
-  DOUBLE: 0n, // BigInt
+  DOUBLE: 0n,
   STRING: 1n,
   EXTRA: 2n,
-  POINTER: 3n,
+  DSGO: 3n, 
   CALC: 4n,
+}
+
+// Internal values for state machine only
+const State = {
+  ...DsgoType,
+
+  HEADER: 100n,
+  NODE: 101n,
+}
+
+function decompileDsgo(_, buffer, config) {
+  function abort(msg) {
+    console.log(crawler)
+    throw new Error(msg)
+  }
+
+  let state = State.HEADER
+  let prev = -1
+
+  const crawler = new BufferCrawler(buffer)
+
+
+  const refs = new ReferenceTracker()
+
+  const variables = []
+  const nodes = []
+  const tables = []
+  const extra = []
+  const data = { tally: crawler.tallyMarks, refs: refs.refs, processed: refs.processed, extra, tables, nodes }
+
+  loop: while(!crawler.isDone()) {
+    if (prev === crawler.index) {
+      console.log(data)
+      throw new Error(`Crawler has not advanced at ${crawler.idx()} (State: ${state})`)
+    }
+    prev = crawler.index
+
+    const ref = refs.peek()
+    if(ref && ref.idx === crawler.index) {
+      state = ref.state
+      refs.consume()
+    } else if(ref) {
+      crawler.skipTo(ref.idx)
+      continue
+    } else if(state == null) {
+      console.log(data)
+      throw new Error(`Crawler orphaned at ${crawler.idx()} (State: ${state})`)
+    }
+
+    switch(state) {
+      // # DSGO Header
+      //
+      // DsRt NcPt  |0x10
+      //
+      // Ds: Leader that says either DSGO or OGSD to indicate big-endian/little-endian
+      // Rt: Points to the top-level DSGO table
+      // Nc: Amount of DSGO nodes
+      // Pt: Pointer to first DSGO node
+      case State.HEADER: {
+        crawler.context = 'DSGO Header'
+        const leader = crawler.ascii(0x00, 0x04)
+        const rootPtr = crawler.ptr(0x04)
+        const nodesCount = crawler.uint(0x08)
+        const nodesPtr = crawler.ptr(0x0c)
+
+        for(let i = 0; i < nodesCount; i++) {
+          refs.add({
+            idx: nodesPtr + i * 0x10,
+            state: State.NODE,
+            origin: crawler.index,
+          })
+        }
+
+        data.header = {
+          leader,
+          rootPtr,
+          nodesCount,
+          nodesPtr,
+        }
+        crawler.endian = leader === 'DSGO' ? 'LE' : 'BE'
+
+        crawler.jump(0x10)
+        break
+      }
+
+      // # Single DSGO node
+      //
+      // DtBg Type | 0x10
+      //
+      // Type: Indicates the type of data in DtBd
+      // 0: DtBg is a 64-bit double
+      // 1: Dt points to a string,         Bg is 0
+      // 2: Dt points to an embedded file, Bg is 0
+      // 3: Dt points to a DSGO list,      Bg is 0
+      // 4: Dt points to a Calc node,      Bg is 0
+      case State.NODE: {
+        crawler.context = count('DSGO Node')
+        const { type, double, ptr } = crawler.dsgo(0x00)
+
+        switch(type) {
+          case DsgoType.DOUBLE: {
+            const node = { idx: crawler.index, type, double }
+            nodes.push(node)
+            break
+          }
+
+          default:
+            const node = { idx: crawler.index, type, ptr }
+            nodes.push(node)
+            refs.add({
+              idx: ptr,
+              state: type,
+              origin: node,
+            })
+        }
+        
+        crawler.jump(0x10)
+        break
+      }
+
+      // # Embedded file
+      // SzDp
+      // ...data...
+      //
+      // Sz: Length of the data in bytes
+      // Dp: Points to the start of the data
+      // 
+      // The file may or may not be in a known format, including SGO, RMP, DSGO, or anything else
+      // Contents of embedded files are independent from this file and will not reference our data
+      // The data is padded up to the nearest 8 bytes
+      case State.EXTRA: {
+        crawler.context = count('Extra')
+        const length = ceil(crawler.uint(0x00), 0x8)
+        const offset = crawler.uint(0x04)
+        const data = crawler.hex(offset, length)
+        extra.push({
+          idx: crawler.index,
+          origin: ref.origin,
+          data,
+        })
+
+        crawler.jump(0x10 + length)
+        break
+      }
+
+      // # DSGO list
+      //
+      // SpSc IcNc
+      // I1I2 ..IN
+      // L1L2 ..LN
+      // S1S2 ..SN
+      //
+      // Sp: Points to structure of node names. May be 0, indicating anonymous nodes or an empty list.
+      // Sc: The count of named nodes. May be 0.
+      // Ic: Points to table of DSGO node indices. May be 0, indicating an empty list.
+      // Nc: The count of nodes being referenced. May be 0 for an empty list.
+      //
+      // I1, I2... IN: Node indexes. Index refers to the order in the file's overall DSGO heap.
+      // L1, L2... LN: Node indexes. If L1 is 8, then the first string names the node mentioned in I8.
+      // S1, S2... SN: Pointer to the string being used. S1 corresponds to the index indexed by L1.
+      case State.DSGO: {
+        const label = count('DSGO Table')
+        crawler.context = label
+        const strCursor = crawler.ptr(0x00)
+        const strCount = crawler.uint(0x04)
+        const varCursor = crawler.ptr(0x08)
+        const varCount = crawler.uint(0x0c)
+
+        const indices = new Array(varCount)
+        const stringIndices = new Array(strCount)
+
+        const table = { idx: crawler.index, strCursor, strCount, varCursor, varCount, strCount, varCount }
+        tables.push(table)
+
+        crawler.jump(0x10)
+        crawler.context = `${label} Index`
+
+        for(let i = 0; i < varCount; i++) {
+          indices[i] = crawler.uint(i + 0x04)
+        }
+
+
+        crawler.jump(0x10 + ceil(strCount, 2) * 0x08 + ceil(varCount, 2) * 0x04)
+        break;
+      }
+
+      default:
+        // break loop
+        console.log(data)
+        throw new Error(`Unknown state ${state} at ${crawler.idx()}`)
+    }
+
+    state = null
+  }
+
+  return data
+}
+
+let countLabels = {}
+function count(label) {
+  countLabels[label] ??= 0
+  return `${label} ${countLabels[label]++}`
+}
+
+function ceil(v, x) {
+  return Math.ceil(v / x) * x
 }
 
 class BufferCrawler { // The intention is to crawl the buffer from start to end, keeping track of every single byte
@@ -35,6 +239,7 @@ class BufferCrawler { // The intention is to crawl the buffer from start to end,
 
   skipTo(index) {
     this.tally(0, 'SKIPPED', index - this.index)
+    this.index = index
   }
 
   isDone() {
@@ -42,8 +247,9 @@ class BufferCrawler { // The intention is to crawl the buffer from start to end,
   }
 
   tally(offset = 0x00, label, length) {
-    for(let idx = this.index + offset; idx < this.index + offset + length; idx += 0x04) 
-      this.tallyMarks.push([this.context, label, idx]);
+    // for(let idx = this.index + offset; idx < this.index + offset + length; idx += 0x04) 
+    //   this.tallyMarks.push([this.context, label, idx]);
+    this.tallyMarks.push([this.context, label, this.index + offset, this.index + offset + length])
   }
 
   ascii(offset = 0x00, length = 0x04) {
@@ -56,8 +262,8 @@ class BufferCrawler { // The intention is to crawl the buffer from start to end,
   uint(offset = 0x00) {
     this.tally(offset, 'uint', 0x04)
     return (this.endian === 'LE')
-      ? val.readUInt32LE()
-      : val.readUInt32BE()
+      ? this.at(offset).readUInt32LE()
+      : this.at(offset).readUInt32BE()
   }
 
   ptr(offset = 0x00) {
@@ -88,7 +294,7 @@ class BufferCrawler { // The intention is to crawl the buffer from start to end,
       : this.at(offset + 0x08).readBigInt64BE()
     let ptr, double
 
-    if(type === 0)
+    if(type === DsgoType.DOUBLE)
       double = this.double(offset + 0x00)
     else {
       ptr = this.ptr(offset + 0x00)
@@ -98,6 +304,29 @@ class BufferCrawler { // The intention is to crawl the buffer from start to end,
 
     return { type, ptr, double }
   }
+
+  hex(offset = 0x00, length = Infinity) {
+    this.tally(offset, 'hex', length)
+    return this.buffer.slice(offset, offset + length).toString('hex')
+  }
+
+  [util.inspect.custom]() {
+    const startAt = Math.max(0, Math.floor((this.index / 0x10) - 1) * 0x10)
+    const endAt = Math.min(startAt + 0x80, this.buffer.length)
+    let bufferView = []
+    for(let i = startAt; i < endAt; i += 0x2) {
+      if(!(i % 0x10)) {
+        bufferView.push([kleur.magenta(`${i.toString(16).padStart(8, 0)}:`)])
+      }
+      let str = this.buffer.readUInt16BE(i).toString(16).padStart(4, 0)
+      if(this.index === i) {
+        str = kleur.yellow(str)
+      }
+      bufferView[bufferView.length - 1].push(str)
+    }
+    return `Cursor 0x${this.index.toString(16)} (${this.endian})
+${bufferView.map(r => r.join(' ')).join('\n')}`
+  }
 }
 
 class ReferenceTracker {
@@ -106,14 +335,14 @@ class ReferenceTracker {
     this.processed = []
   }
 
-  add(idx, mode, origin) { // Adds the reference sorted
+  add({ idx, state, origin }) { // Adds the reference sorted
     let i = 0
     for(; i < this.refs.length; i++) {
       const ref = this.refs[i]
       if(idx < ref.idx)
         break
     }
-    this.refs.splice(i, 0, { idx, mode, origin })
+    this.refs.splice(i, 0, { idx, state, origin })
   }
 
   peek() {
@@ -123,131 +352,6 @@ class ReferenceTracker {
   consume() {
     this.processed.push(this.refs.shift())
   }
-}
-
-function decompileDsgo(_, buffer, config) {
-  let mode = Mode.HEADER
-  let prev = -1
-
-  const crawler = new BufferCrawler(buffer)
-
-  const references = new ReferenceTracker()
-
-  const variables = []
-  const nodes = []
-  const data = { tally: crawler.tallyMarks, refs: references.refs, processed: references.processed, nodes }
-
-  loop: while(!crawler.isDone()) {
-    if (prev === crawler.index) {
-      console.log(data)
-      throw new Error(`Crawler has not advanced at ${crawler.idx()} (Mode: ${mode})`)
-    }
-    prev = crawler.index
-
-    const ref = references.peek()
-    if(ref && ref.idx === crawler.index) {
-      mode = ref.mode
-      references.consume()
-    } else if(ref) {
-      crawler.skipTo(ref.idx)
-      continue
-    } else if(mode == null) {
-      console.log(data)
-      throw new Error(`Crawler orphaned at ${crawler.idx()} (Mode: ${mode})`)
-    }
-
-    switch(mode) {
-      // # DSGO Header
-      //
-      // DSGO AABB
-      //
-      // DSGO: Leader that says either DSGO or OGSD to indicate big-endian/little-endian
-      // AA: Points to the top-level DSGO list
-      // BB: Points to the heap nodes
-      case Mode.HEADER: {
-        crawler.context = 'DSGO Header'
-        const leader = crawler.ascii(0x00, 0x04)
-        const varsPtr = crawler.ptr(0x04)
-        const nodesPtr = crawler.ptr(0x08)
-
-        references.add({
-          idx: varsPtr,
-          mode: Mode.VARS,
-          origin: crawler.index,
-        })
-
-        data.leader = leader
-        crawler.endian = leader === 'DSGO' ? 'LE' : 'BE'
-
-        crawler.jump(0x10)
-        break
-      }
-
-      // # Single DSGO node
-      //
-      // AABB TYPE
-      //
-      // TYPE: Indicates the type of data in AABB
-      // 0: AABB is a 64-bit double
-      // 1: AA points to a string,         BB is 0
-      // 2: AA points to an embedded file, BB is 0
-      // 3: AA points to a DSGO list,      BB is 0
-      // 4: AA points to a Calc node,      BB is 0
-      case Mode.VARS: {
-        crawler.context = ` Node ${nodes.length}`
-        const { type, double, ptr } = crawler.dsgo(0x00)
-        if (type === 0)
-          nodes.push({ type, double })
-        else
-          nodes.push({ type, ptr })
-
-        switch(type) {
-          case DsgoType.POINTER: {
-            references.add({
-              idx: ptr,
-              mode: Mode.DSGO,
-              origin: crawler.index,
-            })
-
-            break
-          }
-
-          default:
-            throw new Error(`Unknown DSGO type: ${type}`)
-        }
-        
-        crawler.jump(0x10)
-        break
-      }
-
-      // # DSGO list
-      //
-      // AABB CCDD
-      //
-      // AA: Points to a list of null-terminated strings that names each DSGO node in order. May be 0 for anonymous node.
-      // BB: The count of strings. May be 0.
-      // CC: Points to the first DSGO node in the array, usually 0x10. Can be 0 for an empty list.
-      // DD: The count of nodes. May be 0 for an empty list.
-      case Mode.DSGO: {
-        const strCursor = crawler.ptr(0x00)
-        const strCount = crawler.uint(0x04)
-        const varCursor = crawler.ptr(0x08)
-        const varCount = crawler.uint(0x0C)
-
-        crawler.jump(0x10)
-        break;
-      }
-
-      default:
-        // break loop
-        console.log(data)
-        throw new Error(`Unknown mode ${mode} at ${crawler.idx()}`)
-    }
-
-    mode = null
-  }
-
-  return data
 }
 
 module.exports = decompileDsgo
