8dc383e6061baaa5922e5237056ac1e61d7d8585
Tony Sæle <tony.sale@linkmobility.com>
2026-03-04 09:47:54 +0100

    clutter

--- converters/dsgo/to-json.js
+++ converters/dsgo/to-json.js
@@ -2,7 +2,7 @@ const { dsgoToBlocks } = require('./dsgo-to-blocks')
 const { dsgoBlocksToJson } = require('./dsgo-blocks-to-json')
 
 function decompileDsgo(buffer, opts) {
-  const blocks = dsgoToDsgoBlocks(buffer, opts)
+  const blocks = dsgoToBlocks(buffer, opts)
   if(opts.blocks)
     return blocks
   const resolved = dsgoBlocksToJson(blocks, opts)
