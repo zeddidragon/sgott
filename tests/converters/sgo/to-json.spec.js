5ba24d511f83086019f76999611faa6d67c4546c
Tony Sæle <tony.sale@linkmobility.com>
2026-03-11 22:21:55 +0100

    test each node type

--- tests/converters/sgo/to-json.spec.js
+++ tests/converters/sgo/to-json.spec.js
@@ -47,7 +47,6 @@ describe('sgo\\to-json.js', () => {
 
     varNames.slice(0x00).writeUint32BE(varNames.length) // String is first item after this buffer
     varNames.slice(0x04).writeUint32BE(0) // first item
-    console.log()
 
     const buffer = Buffer.concat([
       header,
@@ -65,7 +64,7 @@ describe('sgo\\to-json.js', () => {
     })
   })
 
-  it('should parse int', t => {
+  it('should parse 1:int', t => {
     const setup = setupSimpleSgo()
     const node = setup[1]
 
@@ -84,7 +83,7 @@ describe('sgo\\to-json.js', () => {
     })
   })
 
-  it('should parse float', t => {
+  it('should parse 2:float', t => {
     const setup = setupSimpleSgo()
     const node = setup[1]
 
@@ -100,6 +99,123 @@ describe('sgo\\to-json.js', () => {
       endian: 'LE',
       version: 258,
       variables: [ { name: 'test', type: 'float', value: 9.5 } ],
+    })
+  })
+
+  it('should parse 0:ptr', t => {
+    const [header, nodes, names, strings] = setupSimpleSgo()
+
+    nodes.slice(0x00).writeUint32LE(0) // type: ptr
+    nodes.slice(0x04).writeUint32LE(2) // amount of items being pointed to
+    nodes.slice(0x08).writeUint32LE(nodes.length + names.length + strings.length) // Heap is not normally after strings, but there is no reason the parser should not handle this
+
+    const heap = Buffer.alloc(0x0C * 2) // The items being pointed to
+    let node = heap.slice(0x00) // First node
+    node.slice(0x00).writeUint32LE(1)
+    node.slice(0x04).writeUint32LE(0x04)
+    node.slice(0x08).writeUint32LE(3)
+
+    node = heap.slice(0x0C)    // Second node
+    node.slice(0x00).writeUint32LE(2)
+    node.slice(0x04).writeUint32LE(0x04)
+    node.slice(0x08).writeFloatLE(3.5)
+
+    const buffer = Buffer.concat([
+      header,
+      nodes,
+      names,
+      strings,
+      heap,
+    ])
+    const result = decompileSgo(decompiler, buffer)
+
+    t.assert.deepEqual(result, {
+      format: 'SGO',
+      endian: 'LE',
+      version: 258,
+      variables: [ { name: 'test', type: 'ptr', value: [
+        { type: 'int', value: 3 },
+        { type: 'float', value: 3.5 },
+      ] } ],
+    })
+  })
+
+  it('should parse 3:string', t => {
+    const [header, nodes, names, strings] = setupSimpleSgo()
+
+    const string = 'We shall never surrender, we are the E D F!'
+    nodes.slice(0x00).writeUint32LE(3) // type: string
+    nodes.slice(0x04).writeUint32LE(string.length) // length of string being pointed to (before terminator)
+    nodes.slice(0x08).writeUint32LE(nodes.length + names.length + strings.length)
+
+    const strings2 = Buffer.from(string + '\0', 'utf16le')
+
+    const buffer = Buffer.concat([
+      header,
+      nodes,
+      names,
+      strings,
+      strings2,
+    ])
+    const result = decompileSgo(decompiler, buffer)
+
+    t.assert.deepEqual(result, {
+      format: 'SGO',
+      endian: 'LE',
+      version: 258,
+      variables: [ { name: 'test', type: 'string', value: string } ],
+    })
+  })
+
+  it('should parse 4:extra', t => {
+    const [header, nodes, names, strings] = setupSimpleSgo()
+
+    const extra = Buffer.from('Mystery content\0', 'ascii')
+
+    nodes.slice(0x00).writeUint32LE(4) // type: extra
+    nodes.slice(0x04).writeUint32LE(extra.length) // length of data being pointed to
+    nodes.slice(0x08).writeUint32LE(nodes.length + names.length + strings.length) // extra is not normally after strings, but the parser will handle it fine
+
+    const buffer = Buffer.concat([
+      header,
+      nodes,
+      names,
+      strings,
+      extra,
+    ])
+    const result = decompileSgo(decompiler, buffer)
+
+    t.assert.deepEqual(result, {
+      format: 'SGO',
+      endian: 'LE',
+      version: 258,
+      variables: [ { name: 'test', type: 'extra', value: extra.toString('base64') } ],
+    })
+  })
+
+  it('should parse 5:extra', t => {
+    const [header, nodes, names, strings] = setupSimpleSgo()
+
+    const extra = Buffer.from('Mystery content\0', 'ascii')
+
+    nodes.slice(0x00).writeUint32LE(5) // type: extra5
+    nodes.slice(0x04).writeUint32LE(extra.length) // length of data being pointed to
+    nodes.slice(0x08).writeUint32LE(nodes.length + names.length + strings.length) // extra is not normally after strings, but the parser will handle it fine
+
+    const buffer = Buffer.concat([
+      header,
+      nodes,
+      names,
+      strings,
+      extra,
+    ])
+    const result = decompileSgo(decompiler, buffer)
+
+    t.assert.deepEqual(result, {
+      format: 'SGO',
+      endian: 'LE',
+      version: 258,
+      variables: [ { name: 'test', type: 'extra5', value: extra.toString('base64') } ],
     })
   })
 })
