88d9e57efb1e0dc8bc1d9bebb935dcf4d77867d3
Tony Sæle <tony.sale@linkmobility.com>
2026-04-08 09:01:59 +0200

    reduce diff

--- converters/dsgo/compile-blocks/dsgo-extra.js
+++ converters/dsgo/compile-blocks/dsgo-extra.js
@@ -8,16 +8,16 @@
 // The file may or may not be in a known format, including SGO, RMP, DSGO, or anything else
 // Contents of embedded files are independent from this file and will not reference our data
 // The data is padded up to the nearest 8 bytes
-function dsgoExtra(writer, value) {
-  if(value.format !== 'hex')
-    throw new Error('format should be hex')
+function dsgoExtra(writer, { format, value}) {
+  if(format !== 'hex')
+    throw new Error(`format should be "hex", but was "${format}"`)
 
   // Extra data wants to be aligned at the next 0
   const offset = Math.ceil((writer.address + 0x08) / 0x10) * 0x10 - writer.address
-  writer.uint(0x00, value.value.length / 2) // A byte is 2 hex characters
+  writer.uint(0x00, value.length / 2) // A byte is 2 hex characters
   writer.uint(0x04, offset) // Relative pointer to content body
   writer.jump(offset)
-  writer.hex(0x00, value.value)
+  writer.hex(0x00, value)
 }
 
 module.exports = {
